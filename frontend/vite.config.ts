8adaad38f98c9f0019e9be63a17cadc5bf252251
Titya Yem <thitya.yem.photo@gmail.com>
2025-10-14 19:05:15 -0400

    Fix: bugs when deploy on vercel

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,4 @@
-/// <reference types="vitest" />
-import { defineConfig } from "vitest/config";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
@@ -12,10 +11,5 @@ export default defineConfig({
       "@": path.resolve(__dirname, "src"),
       "@assets": path.resolve(__dirname, "src/assets"),
     },
-  },
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "./tests/setup.ts",
   },
 });
