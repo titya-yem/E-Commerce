b1576410db40bef45cf0d6c1fbb409d19ccedbe8
Titya Yem <thitya.yem.photo@gmail.com>
2025-10-15 21:15:30 -0400

    Fix: add missing dashboard SVG

--- frontend/src/components/dashboard/WelcomeDashboard.tsx
+++ frontend/src/components/dashboard/WelcomeDashboard.tsx
@@ -1,29 +1,34 @@
-import { Box, Text } from '@radix-ui/themes'
-import DashboardImage from '@/assets/image/Dashboard-Image.png'
-import DashboardBackgroundImage from '@/assets/image/Dashboard-Background-Image.jpg'
+import { Box, Text } from "@radix-ui/themes";
+import DashboardImage from "@/assets/image/Dashboard-Image.png";
+import DashboardBackgroundImage from "@/assets/image/Dashboard-Background-Image.jpg";
 
 const WelcomeDashboard = () => {
-return (
-  <div className="md:h-44 relative flex justify-between items-center space-y-8 p-4 shadow-md rounded-lg bg-[#2954e4]">
-    <Box className='w-3/6 md:w-3/6 2xl:w-3/5'>
-      <Text as="p" className="text-lg md:text-2xl font-medium text-white"> 
-        Welcome to Your Dashboard!
-      </Text>
-      <Text as="p" className="text-sm text-gray-200 mt-2">
-        Here you can manage your projects, view analytics, and customize your settings.
-      </Text>
-    </Box>
+  return (
+    <div className="md:h-44 relative flex justify-between items-center space-y-8 p-4 shadow-md rounded-lg bg-[#2954e4]">
+      <Box className="w-3/6 md:w-3/6 2xl:w-3/5">
+        <Text as="p" className="text-lg md:text-2xl font-medium text-white">
+          Welcome to Your Dashboard!
+        </Text>
+        <Text as="p" className="text-sm text-gray-200 mt-2">
+          Here you can manage your projects, view analytics, and customize your
+          settings.
+        </Text>
+      </Box>
 
-    <Box>
-      <img src={DashboardBackgroundImage} alt="Dashboard Background" 
-        className="absolute right-0 bottom-0 w-full h-full rounded-lg opacity-20"
-      />
-      <img src={DashboardImage} alt="Dashboard Image" 
-        className="absolute right-0 bottom-0 w-56 md:w-60"
-      />
-    </Box>
-  </div>
-)
-}
+      <Box>
+        <img
+          src={DashboardBackgroundImage}
+          alt="Dashboard Background"
+          className="absolute right-0 bottom-0 w-full h-full rounded-lg opacity-20"
+        />
+        <img
+          src={DashboardImage}
+          alt="Dashboard Image"
+          className="absolute right-0 bottom-0 w-56 md:w-60"
+        />
+      </Box>
+    </div>
+  );
+};
 
-export default WelcomeDashboard
+export default WelcomeDashboard;
